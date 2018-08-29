a813c17363bd35421c1db15744db8f67f844ddfb
Trppyxd <33310226+Trppyxd@users.noreply.github.com>
2018-08-29 15:39:54 +0300

    Added NewXML Class
    XmlWriter

---
+++ NoteApplication/NewXML.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml;
+
+namespace NoteApplication
+{
+    class NewXML
+    {
+        public void New()
+        {
+            XmlWriter xmlWriter = XmlWriter.Create("NoteList.xml");
+
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("NoteList");
+            xmlWriter.WriteStartElement("Note");
+            xmlWriter.WriteAttributeString("title", "ExampleTitle");
+            xmlWriter.WriteAttributeString("content", "ExampleContent");
+            xmlWriter.WriteEndElement();
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Close();
+        }
+    }
+}
