a7e6052a793dd9cefa645d22e6bf351b937a5c76
Trppyxd <33310226+Trppyxd@users.noreply.github.com>
2018-08-29 16:48:13 +0300

    Added DeleteNote class + Edited Program.cs

---
+++ NoteApplication/DeleteNote.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml;
+
+namespace NoteApplication
+{
+    class DeleteNote
+    {
+        public void Delete()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("NoteList.xml");
+
+            Console.WriteLine("Enter the prefix of the note you wish to delete: ");
+            int i = 1;
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                Console.WriteLine(i.ToString() + "[{0}] ", i + node.Attributes["title"].InnerText);
+                i++;
+            }
+            int input = int.Parse(Console.ReadLine());
+            XmlNode root = doc.DocumentElement;
+
+            root.RemoveChild(root.ChildNodes[input]);
+            doc.Save("NoteList.xml");
+        }
+    }
+}
--- NoteApplication/Program.cs
+++ NoteApplication/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+
 
 namespace NoteApplication
 {
@@ -10,6 +8,41 @@ namespace NoteApplication
     {
         static void Main(string[] args)
         {
+            if (!File.Exists("NoteList.xml"))
+            {
+                var xml = new NewXML();
+                xml.New();
+            }
+            while (true)
+            {
+                Console.WriteLine("[1] New Note\n[2] Delete Note\n[3] View Note/s\n[4] Exit application");
+                Console.WriteLine("Enter a valid option: ");
+                string input = Convert.ToString(Console.ReadLine());
+
+                if (input == "1" || input == "New")
+                {
+                    var newNote = new NewNote();
+                    newNote.New();
+                }
+                else if (input == "2" || input == "Delete")
+                {
+                    var deleteNote = new DeleteNote();
+                    deleteNote.Delete();
+                }
+                else if (input == "3" || input == "View")
+                {
+                    var viewNotes = new ViewNotes();
+                    viewNotes.View();
+                }
+                else if (input == "4" || input == "Exit")
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, Try Again!");
+                }
+            }
         }
     }
 }
